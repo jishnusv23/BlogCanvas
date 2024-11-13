ff52f6f980c473e44213f079cb747d61ee39825a
jishnuvadakara <jishnuv750@gmail.com>
2024-11-13 21:20:57 +0530

    corrected the env url

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,7 +25,7 @@ app.use(morgan('dev'))
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONT_END_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization", "custom-header"],
